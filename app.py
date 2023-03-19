0a46c723efd63e6afefdb7bdd51cf162bf7675a0
debuger83 <35300408+debuger83@users.noreply.github.com>
2023-03-19 19:16:26 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,46 +1,48 @@
-from flask import Flask, render_template
-import asyncio
+from flask import Flask, render_template, request, jsonify
 from playwright.async_api import async_playwright
+import asyncio
 import re
 
 app = Flask(__name__)
 
-async def get_metar(cities):
-    url = "https://global.amo.go.kr/obsMetar/ObsMetar.do"
-    
-    async with async_playwright() as p:
-        browser = await p.chromium.launch(headless=True)
-        context = await browser.new_context()
-        page = await context.new_page()
-        await page.goto(url)
-        await asyncio.sleep(5)
+@app.route('/', methods=['GET'])
+def index():
+    return render_template('index.html')
 
-        # 전체 페이지 내용 가져오기
-        page_content = await page.content()
+@app.route('/metar', methods=['POST'])
+def get_metar():
+    cities = request.form.getlist('cities[]')
+    metar_list = []
+    loop = asyncio.new_event_loop()
+    asyncio.set_event_loop(loop)
 
-        results = []
-        for city in cities:
-            # 정규 표현식으로 도시이름이 포함된 줄 찾기
-            pattern = rf'<td>{city}<\/td>.*?<td.*?>(.*?)<\/td>'
-            result = re.search(pattern, page_content, re.DOTALL)
+    async def fetch_metar(cities):
+        async with async_playwright() as p:
+            browser = await p.chromium.launch(headless=True)
+            context = await browser.new_context()
+            page = await context.new_page()
+            await page.goto("https://global.amo.go.kr/obsMetar/ObsMetar.do")
+            await asyncio.sleep(5)
+            page_content = await page.content()
 
-            if result:
-                metar_data = result.group(1)
-                results.append((city, metar_data))
-            else:
-                results.append((city, f"{city} 결과를 찾을 수 없습니다."))
+            for city in cities:
+                # 정규 표현식으로 도시이름이 포함된 줄 찾기
+                pattern = rf'<td>{city}<\/td>.*?<td.*?>(.*?)<\/td>'
+                result = re.search(pattern, page_content, re.DOTALL)
 
-        # 리소스 정리
-        await context.close()
-        await browser.close()
+                if result:
+                    metar_data = result.group(1)
+                    metar_list.append((city, metar_data))
+                else:
+                    metar_list.append((city, '해당 결과를 찾을 수 없습니다.'))
 
-        return results
+            # 리소스 정리
+            await context.close()
+            await browser.close()
 
-@app.route('/')
-def home():
-    cities = ["포항경주공항", "예천공항", "중원", "서울", "김포공항"]
-    results = asyncio.run(get_metar(cities))
-    return render_template('index.html', results=results)
+    loop.run_until_complete(fetch_metar(cities))
+
+    return jsonify(metar_list)
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+    app.run(host='0.0.0.0', port=8080, debug=True)
