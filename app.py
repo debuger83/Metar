760ed2a359fe4a848767d8b61bf1fd801ca5e42a
debuger83 <35300408+debuger83@users.noreply.github.com>
2023-03-19 18:51:26 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,22 +1,11 @@
-from flask import Flask, render_template, request
 import asyncio
 from playwright.async_api import async_playwright
 import re
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route("/")
-def index():
-    return render_template("index.html")
-
-@app.route("/get_metar", methods=["POST"])
-def get_metar():
-    cities = request.form.getlist("city")
-    metar_list = []
-    asyncio.run(get_metar_data(cities, metar_list))
-    return render_template("result.html", metar_list=metar_list)
-
-async def get_metar_data(cities, metar_list):
+async def get_metar(cities):
     url = "https://global.amo.go.kr/obsMetar/ObsMetar.do"
     
     async with async_playwright() as p:
@@ -29,6 +18,7 @@ async def get_metar_data(cities, metar_list):
         # 전체 페이지 내용 가져오기
         page_content = await page.content()
 
+        result_list = []
         for city in cities:
             # 정규 표현식으로 도시이름이 포함된 줄 찾기
             pattern = rf'<td>{city}<\/td>.*?<td.*?>(.*?)<\/td>'
@@ -36,13 +26,31 @@ async def get_metar_data(cities, metar_list):
 
             if result:
                 metar_data = result.group(1)
-                metar_list.append(f"{city} : {metar_data}")
+                result_list.append((city, metar_data))
             else:
-                metar_list.append(f"{city} 결과를 찾을 수 없습니다.")
+                result_list.append((city, f"{city} 결과를 찾을 수 없습니다."))
 
         # 리소스 정리
         await context.close()
         await browser.close()
 
+        return result_list
+
+
+@app.route('/', methods=['GET'])
+def index():
+    return render_template('index.html')
+
+
+@app.route('/get_metar', methods=['POST'])
+def get_metar_route():
+    cities = request.form.getlist('city')
+    loop = asyncio.new_event_loop()
+    result = loop.run_until_complete(get_metar(cities))
+    loop.close()
+
+    return render_template('result.html', result=result)
+
+
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=8080)
+    app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
