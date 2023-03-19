fe14a65931380207148e27df4fbe360c23f4bc95
debuger83 <35300408+debuger83@users.noreply.github.com>
2023-03-19 18:29:39 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,26 +1,27 @@
-import os
-from flask import Flask, render_template, request, jsonify
-import requests
-from bs4 import BeautifulSoup
+from flask import Flask, render_template, request
+from requests_html import HTMLSession
 
 app = Flask(__name__)
 
-@app.route("/")
+def get_gimpo_metar():
+    session = HTMLSession()
+    url = 'https://global.amo.go.kr/obsMetar/ObsMetar.do'
+
+    response = session.get(url)
+    response.html.render(sleep=2)
+
+    metar_list = response.html.find('#obsList > tr')
+
+    for metar in metar_list:
+        if '김포공항' in metar.text:
+            return metar.text
+
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template("index.html")
+    if request.method == 'POST':
+        gimpo_metar = get_gimpo_metar()
+        return render_template('index.html', gimpo_metar=gimpo_metar)
+    return render_template('index.html')
 
-@app.route("/get_metar", methods=["POST"])
-def get_metar():
-    icao_code = request.form.get("icao_code")
-    metar_url = f"https://tgftp.nws.noaa.gov/data/observations/metar/stations/{icao_code}.TXT"
-    
-    response = requests.get(metar_url)
-    
-    if response.status_code == 200:
-        metar_text = response.text.strip()
-        return jsonify({"status": "success", "metar": metar_text})
-    else:
-        return jsonify({"status": "error", "message": "Unable to fetch METAR data"})
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+if __name__ == '__main__':
+    app.run(debug=True)
